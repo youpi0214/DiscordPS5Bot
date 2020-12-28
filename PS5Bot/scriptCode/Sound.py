e66192a39afc71d3f5dd4d508b982bb67fd9fc3f
youpi0214 <55603818+youpi0214@users.noreply.github.com>
2020-12-28 06:06:58 -0500

    Update Sound.py

--- PS5Bot/scriptCode/Sound.py
+++ PS5Bot/scriptCode/Sound.py
@@ -16,11 +16,10 @@ def lookup():
     url = 'https://www.amazon.ca/dp/B089GSVLPY/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B089GSVLPY&pd_rd_w=gxK3K&pf_rd_p=970b3512-1bd2-4757-829e-ab114c5ecd76&pd_rd_wg=0q8nc&pf_rd_r=MX7ZQKR3A72BBSHA7R4K&pd_rd_r=d7c9e3d7-b060-437d-ad74-75cbebe477b9&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyUlhYNEVaSU1YOEI2JmVuY3J5cHRlZElkPUEwNzAzNzQ4M0FQOExPV0lZUjg1VSZlbmNyeXB0ZWRBZElkPUEwNjY0MzQ2NUtFQlpFRDFPUzVDJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
     url2 = 'https://www.amazon.ca/Razer-DeathAdder-Wireless-Gaming-Mouse/dp/B08FQMBKQG?ref_=Oct_s9_apbd_onr_hd_bw_b1LRlw3&pf_rd_r=MF59SX6Z89MD8V833K6G&pf_rd_p=5d333356-1349-535e-8f48-5ce3661b66c8&pf_rd_s=merchandised-search-11&pf_rd_t=BROWSE&pf_rd_i=1233055011'
     i = 1
-    session = HTMLSession()
-    while (i < 10):
+    while True:
         try:
             print('try ', i)
-
+            session = HTMLSession()
             r = session.get(url if i % 2 == 0 else url2)
 
             r.html.render(retries=1, timeout=15)
@@ -38,7 +37,7 @@ def lookup():
             print('timeout err')
         except NetworkError:
             print('network err')
-            pass
+            session.close()
         except Exception as e:
             print('unknown err')
             print(e)
